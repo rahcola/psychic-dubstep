5f505a8982a67532f4f5d2d256a736083bf1d40a
Jani Rahkola <jani.rahkola@iki.fi>
2013-06-30 20:28:20 +0300

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject psychic-dupstep "0.1.0-SNAPSHOT"
+  :description ""
+  :license {:name "ISC license"
+            :url "http://www.isc.org/software/license"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/psychic_dupstep/core.clj
@@ -0,0 +1,1 @@
+(ns psychic-dupstep.core)
